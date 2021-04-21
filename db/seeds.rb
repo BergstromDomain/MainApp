de347b9442a8046071488f6154870076dfdfa86b
Niklas Bergstrom <me.niklas@gmail.com>
2021-04-21 10:15:12 +0000

    Added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |person|
+    Person.create!(
+        first_name: "First Name #{person}",
+        last_name: "Last Name #{person}"
+        )
+end
